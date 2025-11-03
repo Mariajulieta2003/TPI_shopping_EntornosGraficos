d21f822db4904e2da234a6d96a68598249baf50f
Julieta Rodriguez <104692472+Mariajulieta2003@users.noreply.github.com>
2025-11-03 04:51:35 -0300

    Update BDDshopping.sql

--- BDDshopping.sql
+++ BDDshopping.sql
@@ -1,3 +1,4 @@
+/*BASE DE DATOS SHOPPING*/
 CREATE DATABASE  IF NOT EXISTS `shopping` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `shopping`;
 -- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
